9b40118790de2674ce98741e57086f8a9ca2ad67
deepaksing <deepak123s456@gmail.com>
2020-05-13 14:33:59 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -22,8 +22,9 @@ class App extends Component {
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
         
-        const api_call = await fetch 
-        (`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=0e922b7f07dee5170635e075362b7a0c&units=metric`);
+        
+    const api_call = await fetch 
+        (`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=0e922b7f07dee5170635e075362b7a0c&units=metric`);
         
         const data = await api_call.json();
         if(city && country) {
@@ -47,6 +48,7 @@ class App extends Component {
                 error:"Please enter the values"
                 })
         }
+         
     }
 
     render() {
